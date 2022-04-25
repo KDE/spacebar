e5b37970eb03237f23d0522ca72ea55300656013
Jonah Brüchert <jbb@kaidan.im>
2022-04-25 23:57:29 +0200

    signal bindings: Improve API

--- src/rs/src/lib.rs
+++ src/rs/src/lib.rs
@@ -2,56 +2,50 @@
 //
 // SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 
-use libsignal_protocol::{
-    Fingerprint, IdentityKey, IdentityKeyPair, KeyPair, PreKeySignalMessage, PrivateKey,
-    ProtocolAddress, PublicKey, SignalMessage, SignalProtocolError,
-};
+use libsignal_protocol as rsp;
 
-use rand::rngs::OsRng;
+use rand_core::OsRng;
 
-pub type SignalResult<T> = Result<T, SignalProtocolError>;
+pub type SignalResult<T> = Result<T, rsp::SignalProtocolError>;
 
-struct SignalKeyPair(KeyPair);
-struct SignalPublicKey(PublicKey);
-struct SignalPrivateKey(PrivateKey);
-struct SignalIdentityKey(IdentityKey);
-struct SignalIdentityKeyPair(IdentityKeyPair);
-struct SignalSignalMessage(SignalMessage);
-struct SignalSignalMessageRef<'a>(&'a SignalMessage);
-struct SignalProtocolAddress(ProtocolAddress);
-struct SignalFingerprint(Fingerprint);
-struct SignalPreKeySignalMessage(PreKeySignalMessage);
+struct KeyPair(rsp::KeyPair);
+struct PublicKey(rsp::PublicKey);
+struct PrivateKey(rsp::PrivateKey);
+struct IdentityKey(rsp::IdentityKey);
+struct IdentityKeyPair(rsp::IdentityKeyPair);
+struct SignalMessage(rsp::SignalMessage);
+struct SignalMessageRef<'a>(&'a rsp::SignalMessage);
+struct ProtocolAddress(rsp::ProtocolAddress);
+struct Fingerprint(rsp::Fingerprint);
+struct PreKeySignalMessage(rsp::PreKeySignalMessage);
 
-impl SignalKeyPair {
-    fn public_key(&self) -> Box<SignalPublicKey> {
-        Box::new(SignalPublicKey(self.0.public_key))
+impl KeyPair {
+    fn public_key(&self) -> Box<PublicKey> {
+        Box::new(PublicKey(self.0.public_key))
     }
 
-    fn private_key(&self) -> Box<SignalPrivateKey> {
-        Box::new(SignalPrivateKey(self.0.private_key))
+    fn private_key(&self) -> Box<PrivateKey> {
+        Box::new(PrivateKey(self.0.private_key))
     }
 }
 
-fn generate_keypair() -> Box<SignalKeyPair> {
-    Box::new(SignalKeyPair(KeyPair::generate(&mut OsRng)))
+fn generate_keypair() -> Box<KeyPair> {
+    Box::new(KeyPair(rsp::KeyPair::generate(&mut OsRng)))
 }
 
-fn load_keypair(
-    public_key: Box<SignalPublicKey>,
-    private_key: Box<SignalPrivateKey>,
-) -> Box<SignalKeyPair> {
-    Box::new(SignalKeyPair(KeyPair::new(public_key.0, private_key.0)))
+fn load_keypair(public_key: Box<PublicKey>, private_key: Box<PrivateKey>) -> Box<KeyPair> {
+    Box::new(KeyPair(rsp::KeyPair::new(public_key.0, private_key.0)))
 }
 
-fn new_identity_key(public_key: Box<SignalPublicKey>) -> Box<SignalIdentityKey> {
-    Box::new(SignalIdentityKey(IdentityKey::new(public_key.0)))
+fn new_identity_key(public_key: Box<PublicKey>) -> Box<IdentityKey> {
+    Box::new(IdentityKey(rsp::IdentityKey::new(public_key.0)))
 }
 
 fn new_identity_key_pair(
-    identity_key: Box<SignalIdentityKey>,
-    private_key: Box<SignalPrivateKey>,
-) -> Box<SignalIdentityKeyPair> {
-    Box::new(SignalIdentityKeyPair(IdentityKeyPair::new(
+    identity_key: Box<IdentityKey>,
+    private_key: Box<PrivateKey>,
+) -> Box<IdentityKeyPair> {
+    Box::new(IdentityKeyPair(rsp::IdentityKeyPair::new(
         identity_key.0,
         private_key.0,
     )))
@@ -60,14 +54,14 @@ fn new_identity_key_pair(
 fn new_signal_message(
     message_version: u8,
     mac_key: &[u8],
-    sender_ratchet_key: Box<SignalPublicKey>,
+    sender_ratchet_key: Box<PublicKey>,
     counter: u32,
     previous_counter: u32,
     ciphertext: &[u8],
-    sender_identity_key: Box<SignalIdentityKey>,
-    receiver_identity_key: Box<SignalIdentityKey>,
-) -> SignalResult<Box<SignalSignalMessage>> {
-    Ok(Box::new(SignalSignalMessage(SignalMessage::new(
+    sender_identity_key: Box<IdentityKey>,
+    receiver_identity_key: Box<IdentityKey>,
+) -> SignalResult<Box<SignalMessage>> {
+    Ok(Box::new(SignalMessage(rsp::SignalMessage::new(
         message_version,
         mac_key,
         sender_ratchet_key.0,
@@ -79,27 +73,27 @@ fn new_signal_message(
     )?)))
 }
 
-impl SignalSignalMessage {
+impl SignalMessage {
     fn body(&self) -> &[u8] {
         self.0.body()
     }
 }
 
-impl<'a> SignalSignalMessageRef<'a> {
+impl<'a> SignalMessageRef<'a> {
     fn body(&self) -> &[u8] {
         self.0.body()
     }
 
-    fn to_owned(&self) -> Box<SignalSignalMessage> {
-        Box::from(SignalSignalMessage(self.0.clone()))
+    fn to_owned(&self) -> Box<SignalMessage> {
+        Box::from(SignalMessage(self.0.clone()))
     }
 }
 
-fn new_protocol_address(name: String, device_id: u32) -> Box<SignalProtocolAddress> {
-    Box::new(SignalProtocolAddress(ProtocolAddress::new(name, device_id)))
+fn new_protocol_address(name: String, device_id: u32) -> Box<ProtocolAddress> {
+    Box::new(ProtocolAddress(rsp::ProtocolAddress::new(name, device_id)))
 }
 
-impl SignalProtocolAddress {
+impl ProtocolAddress {
     fn name(&self) -> &str {
         self.0.name()
     }
@@ -113,11 +107,11 @@ fn new_fingerprint(
     version: u32,
     iterations: u32,
     local_id: &[u8],
-    local_key: &Box<SignalIdentityKey>,
+    local_key: &Box<IdentityKey>,
     remote_id: &[u8],
-    remote_key: &Box<SignalIdentityKey>,
-) -> SignalResult<Box<SignalFingerprint>> {
-    Ok(Box::from(SignalFingerprint(Fingerprint::new(
+    remote_key: &Box<IdentityKey>,
+) -> SignalResult<Box<Fingerprint>> {
+    Ok(Box::from(Fingerprint(rsp::Fingerprint::new(
         version,
         iterations,
         local_id,
@@ -127,7 +121,7 @@ fn new_fingerprint(
     )?)))
 }
 
-impl SignalFingerprint {
+impl Fingerprint {
     fn display_string(&self) -> SignalResult<String> {
         self.0.display_string()
     }
@@ -138,12 +132,12 @@ fn new_pre_key_signal_message_with_pre_key_id(
     registration_id: u32,
     pre_key_id: u32,
     signed_pre_key_id: u32,
-    base_key: Box<SignalPublicKey>,
-    identity_key: Box<SignalIdentityKey>,
-    message: Box<SignalSignalMessage>,
-) -> SignalResult<Box<SignalPreKeySignalMessage>> {
-    Ok(Box::from(SignalPreKeySignalMessage(
-        PreKeySignalMessage::new(
+    base_key: Box<PublicKey>,
+    identity_key: Box<IdentityKey>,
+    message: Box<SignalMessage>,
+) -> SignalResult<Box<PreKeySignalMessage>> {
+    Ok(Box::from(PreKeySignalMessage(
+        rsp::PreKeySignalMessage::new(
             message_version,
             registration_id,
             Some(pre_key_id),
@@ -159,12 +153,12 @@ fn new_pre_key_signal_message(
     message_version: u8,
     registration_id: u32,
     signed_pre_key_id: u32,
-    base_key: Box<SignalPublicKey>,
-    identity_key: Box<SignalIdentityKey>,
-    message: Box<SignalSignalMessage>,
-) -> SignalResult<Box<SignalPreKeySignalMessage>> {
-    Ok(Box::from(SignalPreKeySignalMessage(
-        PreKeySignalMessage::new(
+    base_key: Box<PublicKey>,
+    identity_key: Box<IdentityKey>,
+    message: Box<SignalMessage>,
+) -> SignalResult<Box<PreKeySignalMessage>> {
+    Ok(Box::from(PreKeySignalMessage(
+        rsp::PreKeySignalMessage::new(
             message_version,
             registration_id,
             None,
@@ -176,7 +170,7 @@ fn new_pre_key_signal_message(
     )))
 }
 
-impl SignalPreKeySignalMessage {
+impl PreKeySignalMessage {
     fn message_version(&self) -> u8 {
         self.0.message_version()
     }
@@ -199,16 +193,16 @@ impl SignalPreKeySignalMessage {
         self.0.signed_pre_key_id()
     }
 
-    fn base_key(&self) -> Box<SignalPublicKey> {
-        Box::from(SignalPublicKey(*self.0.base_key()))
+    fn base_key(&self) -> Box<PublicKey> {
+        Box::from(PublicKey(*self.0.base_key()))
     }
 
-    fn identity_key(&self) -> Box<SignalIdentityKey> {
-        Box::from(SignalIdentityKey(*self.0.identity_key()))
+    fn identity_key(&self) -> Box<IdentityKey> {
+        Box::from(IdentityKey(*self.0.identity_key()))
     }
 
-    fn message(&self) -> Box<SignalSignalMessageRef> {
-        Box::from(SignalSignalMessageRef(self.0.message()))
+    fn message(&self) -> Box<SignalMessageRef> {
+        Box::from(SignalMessageRef(self.0.message()))
     }
 
     fn serialized(&self) -> &[u8] {
@@ -216,78 +210,75 @@ impl SignalPreKeySignalMessage {
     }
 }
 
-#[cxx::bridge]
+#[cxx::bridge(namespace = "signal")]
 mod ffi {
     extern "Rust" {
-        type SignalKeyPair;
-        type SignalPrivateKey;
-        type SignalPublicKey;
-        type SignalIdentityKey;
-        type SignalIdentityKeyPair;
-        type SignalSignalMessage;
-        type SignalProtocolAddress;
-        type SignalFingerprint;
-        type SignalPreKeySignalMessage;
-        type SignalSignalMessageRef<'a>;
+        type KeyPair;
+        type PrivateKey;
+        type PublicKey;
+        type IdentityKey;
+        type IdentityKeyPair;
+        type SignalMessage;
+        type ProtocolAddress;
+        type Fingerprint;
+        type PreKeySignalMessage;
+        type SignalMessageRef<'a>;
 
         #[cxx_name = "generateKeypair"]
-        fn generate_keypair() -> Box<SignalKeyPair>;
+        fn generate_keypair() -> Box<KeyPair>;
 
         #[cxx_name = "loadKeypair"]
-        fn load_keypair(
-            public_key: Box<SignalPublicKey>,
-            private_key: Box<SignalPrivateKey>,
-        ) -> Box<SignalKeyPair>;
+        fn load_keypair(public_key: Box<PublicKey>, private_key: Box<PrivateKey>) -> Box<KeyPair>;
 
         #[cxx_name = "privateKey"]
-        fn private_key(self: &SignalKeyPair) -> Box<SignalPrivateKey>;
+        fn private_key(self: &KeyPair) -> Box<PrivateKey>;
 
         #[cxx_name = "publicKey"]
-        fn public_key(self: &SignalKeyPair) -> Box<SignalPublicKey>;
+        fn public_key(self: &KeyPair) -> Box<PublicKey>;
 
         #[cxx_name = "newIdentityKey"]
-        fn new_identity_key(public_key: Box<SignalPublicKey>) -> Box<SignalIdentityKey>;
+        fn new_identity_key(public_key: Box<PublicKey>) -> Box<IdentityKey>;
 
         #[cxx_name = "newIdentityKeyPair"]
         fn new_identity_key_pair(
-            identity_key: Box<SignalIdentityKey>,
-            private_key: Box<SignalPrivateKey>,
-        ) -> Box<SignalIdentityKeyPair>;
+            identity_key: Box<IdentityKey>,
+            private_key: Box<PrivateKey>,
+        ) -> Box<IdentityKeyPair>;
 
         #[cxx_name = "newSignalMessage"]
         fn new_signal_message(
             message_version: u8,
             mac_key: &[u8],
-            sender_ratchet_key: Box<SignalPublicKey>,
+            sender_ratchet_key: Box<PublicKey>,
             counter: u32,
             previous_counter: u32,
             ciphertext: &[u8],
-            sender_identity_key: Box<SignalIdentityKey>,
-            receiver_identity_key: Box<SignalIdentityKey>,
-        ) -> Result<Box<SignalSignalMessage>>;
+            sender_identity_key: Box<IdentityKey>,
+            receiver_identity_key: Box<IdentityKey>,
+        ) -> Result<Box<SignalMessage>>;
 
-        fn body(self: &SignalSignalMessage) -> &[u8];
+        fn body(self: &SignalMessage) -> &[u8];
 
         #[cxx_name = "newProtocolAddress"]
-        fn new_protocol_address(name: String, device_id: u32) -> Box<SignalProtocolAddress>;
+        fn new_protocol_address(name: String, device_id: u32) -> Box<ProtocolAddress>;
 
-        fn name(self: &SignalProtocolAddress) -> &str;
+        fn name(self: &ProtocolAddress) -> &str;
 
         #[cxx_name = "deviceId"]
-        fn device_id(self: &SignalProtocolAddress) -> u32;
+        fn device_id(self: &ProtocolAddress) -> u32;
 
         #[cxx_name = "newFingerprint"]
         fn new_fingerprint(
             version: u32,
             iterations: u32,
             local_id: &[u8],
-            local_key: &Box<SignalIdentityKey>,
+            local_key: &Box<IdentityKey>,
             remote_id: &[u8],
-            remote_key: &Box<SignalIdentityKey>,
-        ) -> Result<Box<SignalFingerprint>>;
+            remote_key: &Box<IdentityKey>,
+        ) -> Result<Box<Fingerprint>>;
 
         #[cxx_name = "display_string"]
-        fn display_string(self: &SignalFingerprint) -> Result<String>;
+        fn display_string(self: &Fingerprint) -> Result<String>;
 
         #[cxx_name = "newPreKeySignalMessageWithPreKeyId"]
         fn new_pre_key_signal_message_with_pre_key_id(
@@ -295,49 +286,49 @@ mod ffi {
             registration_id: u32,
             pre_key_id: u32,
             signed_pre_key_id: u32,
-            base_key: Box<SignalPublicKey>,
-            identity_key: Box<SignalIdentityKey>,
-            message: Box<SignalSignalMessage>,
-        ) -> Result<Box<SignalPreKeySignalMessage>>;
+            base_key: Box<PublicKey>,
+            identity_key: Box<IdentityKey>,
+            message: Box<SignalMessage>,
+        ) -> Result<Box<PreKeySignalMessage>>;
 
         #[cxx_name = "newPreKeySignalMessage"]
         fn new_pre_key_signal_message(
             message_version: u8,
             registration_id: u32,
             signed_pre_key_id: u32,
-            base_key: Box<SignalPublicKey>,
-            identity_key: Box<SignalIdentityKey>,
-            message: Box<SignalSignalMessage>,
-        ) -> Result<Box<SignalPreKeySignalMessage>>;
+            base_key: Box<PublicKey>,
+            identity_key: Box<IdentityKey>,
+            message: Box<SignalMessage>,
+        ) -> Result<Box<PreKeySignalMessage>>;
 
         #[cxx_name = "messageVersion"]
-        fn message_version(self: &SignalPreKeySignalMessage) -> u8;
+        fn message_version(self: &PreKeySignalMessage) -> u8;
 
         #[cxx_name = "registrationId"]
-        fn registration_id(self: &SignalPreKeySignalMessage) -> u32;
+        fn registration_id(self: &PreKeySignalMessage) -> u32;
 
         #[cxx_name = "hasPreKeyId"]
-        fn has_pre_key_id(self: &SignalPreKeySignalMessage) -> bool;
+        fn has_pre_key_id(self: &PreKeySignalMessage) -> bool;
 
         #[cxx_name = "preKeyId"]
-        fn pre_key_id(self: &SignalPreKeySignalMessage) -> u32;
+        fn pre_key_id(self: &PreKeySignalMessage) -> u32;
 
         #[cxx_name = "signedPreKeyId"]
-        fn signed_pre_key_id(self: &SignalPreKeySignalMessage) -> u32;
+        fn signed_pre_key_id(self: &PreKeySignalMessage) -> u32;
 
         #[cxx_name = "baseKey"]
-        fn base_key(self: &SignalPreKeySignalMessage) -> Box<SignalPublicKey>;
+        fn base_key(self: &PreKeySignalMessage) -> Box<PublicKey>;
 
         #[cxx_name = "identityKey"]
-        fn identity_key(self: &SignalPreKeySignalMessage) -> Box<SignalIdentityKey>;
+        fn identity_key(self: &PreKeySignalMessage) -> Box<IdentityKey>;
 
-        fn message(self: &SignalPreKeySignalMessage) -> Box<SignalSignalMessageRef>;
+        fn message(self: &PreKeySignalMessage) -> Box<SignalMessageRef>;
 
-        fn serialized(self: &SignalPreKeySignalMessage) -> &[u8];
+        fn serialized(self: &PreKeySignalMessage) -> &[u8];
 
-        unsafe fn body<'a>(self: &'a SignalSignalMessageRef<'a>) -> &'a [u8];
+        unsafe fn body<'a>(self: &'a SignalMessageRef<'a>) -> &'a [u8];
 
         #[cxx_name = "toOwned"]
-        unsafe fn to_owned<'a>(self: &'a SignalSignalMessageRef) -> Box<SignalSignalMessage>;
+        unsafe fn to_owned<'a>(self: &'a SignalMessageRef) -> Box<SignalMessage>;
     }
 }
