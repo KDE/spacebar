KDE/spacebar

commit bf7951bfd16ad7061ae6c77114461c69d630648c

Initialize KCrash
To enable crash reporting

daemon/main.cpp
@@ -5,20 +5,27 @@
 #include <QCommandLineParser>
 #include <QCoreApplication>
 
+#include <KAboutData>
+#include <KCrash>
 #include <KDBusService>
 #include <KLocalizedString>
 
+#include "../version.h"
 #include "channellogger.h"
 #include "global.h"
+
+using namespace Qt::Literals;
 
 int main(int argc, char *argv[])
 {
     QCommandLineParser parser;
 
     QCoreApplication app(argc, argv);
-    QCoreApplication::setOrganizationName(SL("KDE"));
-    QCoreApplication::setOrganizationDomain(SL("kde.org"));
-    QCoreApplication::setApplicationName(SL("Spacebar"));
+
+    KAboutData about(u"spacebar-daemon"_s, u"Spacebar"_s, QStringLiteral(SPACEBAR_VERSION_STRING));
+    KAboutData::setApplicationData(about);
+
+    KCrash::initialize();
 
     KLocalizedString::setApplicationDomain("spacebar");
 

src/main.cpp
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 
 #include <KAboutData>
+#include <KCrash>
 #include <KDBusService>
 #include <KLocalizedContext>
 #include <KLocalizedString>
@@ -65,6 +66,8 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     about.addAuthor(i18n("Smitty van Bodegom"), QString(), QStringLiteral("me@smitop.com"));
     KAboutData::setApplicationData(about);
     QApplication::setWindowIcon(QIcon::fromTheme(QStringLiteral("org.kde.spacebar")));
+
+    KCrash::initialize();
 
     parser.addVersionOption();
     parser.addHelpOption();

.kde-ci.yml
@@ -15,6 +15,7 @@ Dependencies:
     'frameworks/kdbusaddons': '@latest-kf6'
     'frameworks/modemmanager-qt': '@latest-kf6'
     'frameworks/kio': '@latest-kf6'
+    'frameworks/kcrash': '@latest-kf6'
     'libraries/kirigami-addons': '@latest-kf6'
     'libraries/futuresql': '@latest-kf6'
     'third-party/qcoro': '@latest'

CMakeLists.txt
@@ -34,7 +34,7 @@ ecm_setup_version(${PROJECT_VERSION}
 
 ################# Find dependencies #################
 find_package(Qt6 ${QT_MIN_VERSION} REQUIRED COMPONENTS Core Concurrent Gui Qml Quick QuickControls2 Widgets Sql)
-find_package(KF6 ${KF_MIN_VERSION} REQUIRED COMPONENTS Kirigami I18n Contacts People Notifications Config CoreAddons DBusAddons ModemManagerQt KIO WindowSystem)
+find_package(KF6 ${KF_MIN_VERSION} REQUIRED COMPONENTS Kirigami I18n Contacts People Notifications Config CoreAddons DBusAddons ModemManagerQt KIO WindowSystem Crash)
 find_package(KF6KirigamiAddons 1.0 REQUIRED)
 find_package(PhoneNumber COMPONENTS PhoneNumber REQUIRED)
 find_package(CURL REQUIRED)

daemon/CMakeLists.txt
@@ -31,6 +31,7 @@ target_link_libraries(spacebar-daemon PRIVATE
     KF6::DBusAddons
     KF6::Notifications
     KF6::ModemManagerQt
+    KF6::Crash
     database
     kTelephonySupport
     macros

src/CMakeLists.txt
@@ -36,6 +36,7 @@ target_link_libraries(spacebar PRIVATE
     KF6::PeopleBackend
     KF6::KIOGui
     KF6::WindowSystem
+    KF6::Crash
     database
     kTelephonySupport
     macros

commit 28967771ce0e46c52592b694a6c5e12997d13966

daemon: Fixup SMS sending
Commit bf7951bfd16a "Initialize KCrash" broke SMS sending with Spacebar
as the name of the Daemon doesn't match what is expected.

Let's revert the name change to keep things consistent.

Fixes: bf7951bfd16a "Initialize KCrash"
Resolves: https://bugs.kde.org/show_bug.cgi?id=494381
Signed-off-by: Alistair Francis <alistair@alistair23.me>

daemon/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 
     QCoreApplication app(argc, argv);
 
-    KAboutData about(u"spacebar-daemon"_s, u"Spacebar"_s, QStringLiteral(SPACEBAR_VERSION_STRING));
+    KAboutData about(u"Spacebar"_s, u"Spacebar"_s, QStringLiteral(SPACEBAR_VERSION_STRING));
     KAboutData::setApplicationData(about);
 
     KCrash::initialize();